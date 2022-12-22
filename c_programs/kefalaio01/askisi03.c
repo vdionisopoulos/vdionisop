eae0811bdd2787dc06d2bdad52fc0dd7f2f58172
Vassilis Dionisopoulos <bdionisogr@gmail.com>
2022-12-22 19:15:34 +0000

    My first attempt

---
+++ c_programs/kefalaio01/askisi03.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+/*this program output the following lines*/
+
+int main(void)
+{
+    printf("This is a program \nwritten in C and\nit produces 4 lines\non the screen\n");
+    return 0;
+}
